0cc028b66e7bed41d49eaf64289f9cd60f6f96a4
FolMikl <foldiak.miklos@gmail.com>
2021-03-21 20:35:21 +0100

    js gyakorlás első rész

---
+++ HajosTeszt2/HajosTeszt2/wwwroot/jsgyak.js
@@ -0,0 +1,3 @@
+
+Valamiért, amikor ide átraktam a script tag-en belüli dolgokat ide, akkor az elsõ rész sem mûködött. Próbáltam, hogy hozzáadom, az onload-hoz, akkor nem hiba üzenetet dobott vissza az F12-õs résznél, de nem látszódott semmi.
+Illetve a css-nél olyan, mintha csak részben mûködne, tehát érzékeli az id-t, de a flex tulajdonságot már nem rakja át sorba.
