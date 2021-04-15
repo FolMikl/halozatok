032f428e28816e947dc417dd4f759a7f44bfa6de
FolMikl <foldiak.miklos@gmail.com>
2021-04-15 16:10:27 +0200

    8. heti feladatok

--- HajosTeszt2/HajosTeszt2/Startup.cs
+++ HajosTeszt2/HajosTeszt2/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt2
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,10 @@ namespace HajosTeszt2
             //        await context.Response.WriteAsync("Hello World!");
             //    });
             //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
